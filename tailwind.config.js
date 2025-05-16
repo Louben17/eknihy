421d3122e2eb08ffed3bf2b3d8525d8060d0b1ab
Jakub Kozel <louben17@gmail.com>
2025-05-16 11:57:52 +0200

    test: zmena pro vercel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
-  content: [],
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx}",  // Next.js app router složka
+    "./src/components/**/*.{js,ts,jsx,tsx}", // pokud máte komponenty
+    "./src/pages/**/*.{js,ts,jsx,tsx}", // pokud používáte pages router
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
